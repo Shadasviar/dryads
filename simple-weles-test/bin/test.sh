13eab998be16142535d37563b3bc75fd83d5efb7
Shadasviar <shadasviarus@gmail.com>
2019-02-22 23:05:28 +0100

    Fix test scenario and write test result directly to file

--- simple-weles-test/bin/test.sh
+++ simple-weles-test/bin/test.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-function runtest    {
-    printf "$1 ... "
+function runtest {
+    printf "$1 ... " >> $3
     $2 2>/dev/null 1>/dev/null
     if [ $? -eq 0 ]; then
-        echo "OK"
+        echo "OK" >> $3
     else
-        echo "FAILED"
+        echo "FAILED" >> $3
     fi
 }
 
-    runtest "SSH available" "ssh -V"
-    runtest "Get kernel info" "uname -a"
-    runtest "GCC available" "gcc -v"
-    runtest "G++ available" "g++ -v"
-    runtest "Get user name" "whoami"
-    runtest "Network is available" "ping 127.0.0.1 -c 3"
-    runtest "Make is available" "make -v"
-    runtest "PS check" "ps"
-    runtest "Vim available" "vim --version"
+    runtest "SSH available" "ssh -V" $1
+    runtest "Get kernel info" "uname -a" $1
+    runtest "GCC available" "gcc -v" $1
+    runtest "G++ available" "g++ -v" $1
+    runtest "Get user name" "whoami" $1
+    runtest "Network is available" "ping 127.0.0.1 -c 3" $1
+    runtest "Make is available" "make -v" $1
+    runtest "PS check" "ps" $1
+    runtest "Vim available" "vim --version" $1
