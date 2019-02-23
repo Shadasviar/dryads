c36d6c806724fcc95fab4bfc4282d578a080fc3e
Shadasviar <shadasviarus@gmail.com>
2019-02-23 16:31:07 +0100

    Make dut_boot better

--- vm-vm_ubuntu/dut_boot.sh
+++ vm-vm_ubuntu/dut_boot.sh
@@ -3,4 +3,11 @@
 . dut_config.sh
 
 sshpass -p vagrant ssh vagrant@${DUT_ADDR} sudo reboot now
-sleep 20
+for i in {1..30}; do
+    ping ${DUT_ADDR} -c1 -w1
+    if [ $? -eq 0 ]; then
+        exit 0
+    fi
+    printf "."
+done
+exit -1
