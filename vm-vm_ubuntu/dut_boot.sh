3b3159cecb7329da4fecb2512655d2d646de02b3
Shadasviar <shadasviarus@gmail.com>
2019-02-23 16:45:33 +0100

    Increase timeout of boot

--- vm-vm_ubuntu/dut_boot.sh
+++ vm-vm_ubuntu/dut_boot.sh
@@ -4,8 +4,8 @@
 
 sshpass -p vagrant ssh vagrant@${DUT_ADDR} sudo reboot now
 sleep 10
-for i in {1..30}; do
-    ping ${DUT_ADDR} -c1 -w1
+for i in {1..100}; do
+    ping ${DUT_ADDR} -c1 -w1 >/dev/null
     if [ $? -eq 0 ]; then
         exit 0
     fi
