601cba348fc456d8a8d7cbb58a935ec6b188adfe
Shadasviar <shadasviarus@gmail.com>
2019-02-23 16:52:00 +0100

    dut_login try a lot of time

--- vm-vm_ubuntu/dut_login.sh
+++ vm-vm_ubuntu/dut_login.sh
@@ -4,4 +4,10 @@ yes | ssh-keygen -N "" -t rsa -f /tmp/id_rsa
 
 . dut_config.sh
 
-sshpass -p vagrant ssh-copy-id vagrant@${DUT_ADDR}
+for i in {1..30}; do
+    sshpass -p vagrant ssh-copy-id vagrant@${DUT_ADDR}
+    if [ $? -eq 0 ]; then
+        exit 0
+    fi
+    sleep 1
+done
